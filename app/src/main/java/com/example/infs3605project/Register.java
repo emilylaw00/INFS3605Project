bae83856ad773532f6283fe3d8da40d8f802a743
emilylaw00 <56818095+emilylaw00@users.noreply.github.com>
2020-07-03 17:02:49 +1000

    login stuff

--- app/src/main/java/com/example/infs3605project/Register.java
+++ app/src/main/java/com/example/infs3605project/Register.java
@@ -20,22 +20,26 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
-    EditText mUsername, mEmail, mPassword;
+    EditText mEmail, mPassword, mFirstName, mLastName, mConfirmPassword;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mUsername = findViewById(R.id.username);
+        //initialise
         mEmail = findViewById(R.id.eMail);
         mPassword = findViewById(R.id.password);
+        mConfirmPassword = findViewById(R.id.confirmPassword);
         mRegisterBtn = findViewById(R.id.registerBtn);
         mLoginBtn = findViewById(R.id.createText);
+        mFirstName = findViewById(R.id.firstName);
+        mLastName = findViewById(R.id.lastName);
 
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
@@ -43,29 +47,35 @@ public class Register extends AppCompatActivity {
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
-        }
-
+        } //checks if the user is already logged in so they don't have to see the registration
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                String email = mEmail.getText().toString().trim();
+                String email = mEmail.getText().toString().trim(); //collect email text and trim
                 String password = mPassword.getText().toString().trim();
+                String confirmPassword = mConfirmPassword.getText().toString().trim();
 
-                if(TextUtils.isEmpty(email)){
+                if(TextUtils.isEmpty(email)){ //checks to make sure email is not empty
                     mEmail.setError("Email is Required.");
                     return;
                 }
 
-                if(TextUtils.isEmpty(password)){
+                if(TextUtils.isEmpty(password)){ //checks to make sure password is not empty
                     mPassword.setError("Password is required.");
                     return;
                 }
 
                 if(password.length() < 6){
-                    mPassword.setError("Password must be => 6 characters");
+                    mPassword.setError("Password must 6 or more characters");
                     return;
                 }
+
+                if(!password.equals(confirmPassword)) { // if
+                    mConfirmPassword.setError("Passwords do not match");
+                    return;
+                }
+
                 progressBar.setVisibility(View.VISIBLE);
 
                 //REGISTER THE USER IN FIREBASE
@@ -73,21 +83,24 @@ public class Register extends AppCompatActivity {
                 fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        //in here you check if the registration is successful or not
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this,"User Created.", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class)); //take users to the main activity
                         }else{
-                            Toast.makeText(Register.this,"Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Register.this,"An Error has occurred. User not created" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
 
+        //for if user already has an account
+
         mLoginBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startActivity(new Intent(getApplicationContext(),Login.class));
+                startActivity(new Intent(getApplicationContext(),Login.class)); //takes the user to the login page
             }
         });
 
---
+++ app/src/main/java/com/example/infs3605project/Users.java
@@ -0,0 +1,91 @@
+package com.example.infs3605project;
+
+public class Users {
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth; //to find age for data analytics
+    private String degree;
+    private int purityScore;
+    private int profilePicture;
+    private String howDidYouHear;
+    private String userType;
+
+
+    public Users() {
+  
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public int getPurityScore() {
+        return purityScore;
+    }
+
+    public void setPurityScore(int purityScore) {
+        this.purityScore = purityScore;
+    }
+
+    public int getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(int profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getHowDidYouHear() {
+        return howDidYouHear;
+    }
+
+    public void setHowDidYouHear(String howDidYouHear) {
+        this.howDidYouHear = howDidYouHear;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
