3087948bfd4d083da2684d1a001ae03ae2d6af29
emilylaw00 <56818095+emilylaw00@users.noreply.github.com>
2020-07-24 14:44:33 +1000

    Purity test save functionality implemented

--- app/src/main/java/com/example/infs3605project/ProfileFragment.java
+++ app/src/main/java/com/example/infs3605project/ProfileFragment.java
@@ -1,19 +1,64 @@
 package com.example.infs3605project;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
 public class ProfileFragment extends Fragment {
+
+    TextView mUserName, mUserEmail, textV;
+    ImageView mProfilePic;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mUserName = v.findViewById(R.id.userName);
+        mUserEmail = v.findViewById(R.id.userEmail);
+        mProfilePic = v.findViewById(R.id.profilePic);
+        textV = v.findViewById(R.id.textView);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+        Log.d("PROFILE", "USERID: " + userId);
+
+        //assign retrieved data using getProperties
+
+        DocumentReference dr = fStore.collection("Users").document(userId);
+        dr.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                //update the data
+                mUserEmail.setText(documentSnapshot.getString("email"));
+                mUserName.setText(documentSnapshot.getString("firstName"));
+            }
+        });
+        return v;
     }
 }
