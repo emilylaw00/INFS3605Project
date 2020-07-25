36e4720103fae612ab996f26b01f540b8689562d
emilylaw00 <56818095+emilylaw00@users.noreply.github.com>
2020-07-25 22:56:03 +1000

    NewsAPI done

--- app/src/main/java/com/example/infs3605project/HomeFragment.java
+++ app/src/main/java/com/example/infs3605project/HomeFragment.java
@@ -9,11 +9,128 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.infs3605project.model.Articles;
+import com.example.infs3605project.model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class HomeFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+    EditText etQuery;
+    Button btnSearch,btnAboutUs;
+    Dialog dialog;
+    final String API_KEY = "08d54cfbf9e24f6d98db5e591a6f019e";
+    NewsAdapter mAdapter;
+    List<Articles>  articles = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        swipeRefreshLayout = v.findViewById(R.id.swipeRefresh);
+        recyclerView = v.findViewById(R.id.recyclerView);
+
+        etQuery = v.findViewById(R.id.etQuery);
+        btnSearch = v.findViewById(R.id.btnSearch);
+        //btnAboutUs = v.findViewById(R.id.aboutUs);
+        dialog = new Dialog(getContext());
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        final String country = getCountry();
+
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson("",country,API_KEY);
+            }
+        });
+        retrieveJson("",country,API_KEY);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etQuery.getText().toString().equals("")){
+                    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                        @Override
+                        public void onRefresh() {
+                            retrieveJson(etQuery.getText().toString(),country,API_KEY);
+                        }
+                    });
+                    retrieveJson(etQuery.getText().toString(),country,API_KEY);
+                }else{
+                    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                        @Override
+                        public void onRefresh() {
+                            retrieveJson("",country,API_KEY);
+                        }
+                    });
+                    retrieveJson("",country,API_KEY);
+                }
+            }
+        });
+
+        return v;
     }
+
+    public void retrieveJson(String query ,String country, String apiKey){
+
+
+        swipeRefreshLayout.setRefreshing(true);
+        Call<Headlines> call;
+        if (!etQuery.getText().toString().equals("")){ //string not empty
+            call= ApiClient.getInstance().getApi().getSpecificData(query,apiKey);
+        }else{
+            call= ApiClient.getInstance().getApi().getHeadlines(country,apiKey);
+        }
+
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() && response.body().getArticles() != null){
+                    swipeRefreshLayout.setRefreshing(false);
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    mAdapter = new NewsAdapter(getContext(),articles);
+                    recyclerView.setAdapter(mAdapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(getContext(), t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
+
 }
