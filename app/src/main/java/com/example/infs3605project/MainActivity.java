5dee9d2c00bc387f0bb9303ecd881e036f1129d2
connieshi116 <56817947+connieshi116@users.noreply.github.com>
2020-07-31 21:15:27 +1000

    lmao

--- app/src/main/java/com/example/infs3605project/MainActivity.java
+++ app/src/main/java/com/example/infs3605project/MainActivity.java
@@ -23,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         //Navigation bar is instantiated here
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
